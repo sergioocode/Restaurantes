ef555ab835cd8d87253b86a29cd8fb74862681e9
sergioocode <sergio.code@hotmail.com>
2026-09-23 01:24:23 +0200

    Centraliza la config de compilación y actualiza los paquetes NuGet.

--- src/Services/Identity/Restaurantes.Identity.Infrastructure/Persistence/Migrations/20260921233433_ResetAuthorizedAccounts.cs
+++ src/Services/Identity/Restaurantes.Identity.Infrastructure/Persistence/Migrations/20260921233433_ResetAuthorizedAccounts.cs
@@ -2,18 +2,17 @@
 
 #nullable disable
 
-namespace Restaurantes.Identity.Infrastructure.Persistence.Migrations
+namespace Restaurantes.Identity.Infrastructure.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class ResetAuthorizedAccounts : Migration
 {
     /// <inheritdoc />
-    public partial class ResetAuthorizedAccounts : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.Sql("DELETE FROM authorized_accounts;");
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder) { }
+        migrationBuilder.Sql("DELETE FROM authorized_accounts;");
     }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder) { }
 }
